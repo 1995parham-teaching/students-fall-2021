fece72b4110d85c803129d2507eec3e1575f4d0f
Parham Alvani <parham.alvani@gmail.com>
2021-12-02 11:17:09 +0330

    feat: simple main for database connection

--- main.go
+++ main.go
@@ -2,15 +2,26 @@ package main
 
 import (
 	"log"
+	"time"
 
 	"github.com/gofiber/fiber/v2"
+	"githuh.com/cng-by-example/students/internal/db"
 	"githuh.com/cng-by-example/students/internal/http/handler"
 	"githuh.com/cng-by-example/students/internal/store"
 )
 
 func main() {
+	db, err := db.New(db.Config{
+		URL:               "mongodb://127.0.0.1:27017",
+		Name:              "sbu",
+		ConnectionTimeout: 10 * time.Second,
+	})
+	if err != nil {
+		log.Fatalf("database connection failed %s", err)
+	}
+
 	hs := handler.Student{
-		Store: store.NewMemoryStudent(),
+		Store: store.NewMongoDBStore(db),
 	}
 
 	app := fiber.New()
